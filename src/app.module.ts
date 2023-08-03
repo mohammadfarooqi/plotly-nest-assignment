8aa11a594467f86e4e0aad0e30e2bbc974d8bdf1
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2023-08-03 00:44:46 -0400

    added typeorm and sqlite3 setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { ProductsModule } from './products/products.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     ProductsModule,
   ],
